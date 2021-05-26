f092da2091bc5fa1b8a709b4b0d3ab5ca76f58ea
Yuyu <ujunglim@naver.com>
2021-05-26 12:58:45 +0800

    add gulp webserver

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,7 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
   pug: {
@@ -8,10 +10,23 @@ const routes = {
   }
 }
 
-export const pug = () => 
+// ======= make tasks ==========
+const clean = () => del(["build"]);
+
+const pug = () => 
   gulp
     .src(routes.pug.src)
     .pipe(gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
-export const dev = gulp.series([pug]);
+const webserver = () => 
+  gulp.src("build").pipe(ws({livereload: true, open: true}));
+
+// ======= make series ===========   
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+const postDev = gulp.series([webserver])
+
+export const dev = gulp.series([prepare, assets, postDev]);
